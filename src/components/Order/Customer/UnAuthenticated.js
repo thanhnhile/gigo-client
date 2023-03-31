09564bb01b8958bd5909c5fb92110bbf8d31297c
thanhnhile <marrythanhnhipl205@gmail.com>
2023-03-31 23:19:07 +0700

    refactor order, forminput, customer info

---
+++ src/components/Order/Customer/UnAuthenticated.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import className from 'classnames/bind';
+import styles from './Customer.module.scss';
+import FormInput from '~/components/Form/FormInput';
+import SelectAddress from '../../SelectAddress';
+import ValidationRegex from '~/utils/validationRegex';
+
+const cx = className.bind(styles);
+
+const UnAuthenticated = ({
+  customer,
+  setCustomer,
+  handleChange,
+  setValidated,
+}) => {
+  const [address, setAddress] = useState(() => {
+    const { provinceId, districtId } = customer;
+    return { provinceId, districtId };
+  });
+  useEffect(() => {
+    setCustomer((prev) => ({ ...prev, ...address }));
+  }, [address, setCustomer]);
+  const formInputs = [
+    {
+      id: 1,
+      type: 'text',
+      name: 'name',
+      placeholder: 'Họ và tên',
+      required: true,
+    },
+    {
+      id: 2,
+      type: 'phone',
+      name: 'phone',
+      placeholder: 'Số điện thoại',
+      required: true,
+      pattern: ValidationRegex.phone.pattern,
+      message: ValidationRegex.phone.message,
+    },
+    {
+      id: 3,
+      type: 'text',
+      name: 'address',
+      placeholder: 'Địa chỉ chi tiết',
+      required: true,
+    },
+  ];
+  return (
+    <>
+      <div className={cx('form-control')}>
+        {formInputs.map((formInput) => (
+          <FormInput
+            key={formInput.id}
+            value={customer[formInput.name]}
+            onChange={handleChange}
+            setValidated={setValidated}
+            {...formInput}
+          />
+        ))}
+      </div>
+      <div className={cx('form-control')}>
+        <h4>Địa chỉ </h4>
+        <SelectAddress address={address} setAddress={setAddress} />
+      </div>
+    </>
+  );
+};
+
+export default UnAuthenticated;
--- src/components/Order/Customer/index.js
+++ src/components/Order/Customer/index.js
@@ -3,107 +3,74 @@ import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { Icon } from '@iconify/react';
 import className from 'classnames/bind';
 import styles from './Customer.module.scss';
-import SelectAddress from '../../SelectAddress';
 import Modal from '~/components/Modal';
 import ListCustomerAddress from '../../Personal/ListCustomerAddress';
 import FormValidation from '~/components/Form/FormValidation';
-import FormInput from '~/components/Form/FormInput';
+import Authenticated from './Authenticated';
+import UnAuthenticated from './UnAuthenticated';
 import useOrder from '~/hooks/useOrder';
 import { httpGetStoreByAddress } from '~/apiServices/storeServices';
 import { httpGetCustomerInfoDefault } from '~/apiServices/accountServices';
-import ValidationRegex from '~/utils/validationRegex';
 
 const cx = className.bind(styles);
 
 const Customer = () => {
   const { customer, setCustomer, accountUsername, handleCheckout } = useOrder();
-  const [address, setAddress] = useState(() => {
-    return { provinceId: customer.provinceId, districtId: customer.districtId };
-  });
   const [stores, setStores] = useState([]);
   const [showModal, setShowModal] = useState(false);
+  const getListStoreByAddress = async (provinceId, districtId) => {
+    let res = [];
+    res = await httpGetStoreByAddress(provinceId, districtId);
+    if (res.data?.length > 0) {
+      setStores(res.data);
+      setCustomer({
+        ...customer,
+        store_id: res.data[0].id,
+      });
+    } else {
+      setStores([{ id: null, storeName: 'Không có cửa hàng tại địa chỉ này' }]);
+    }
+  };
   useEffect(() => {
-    const getCustomerInfo = async () => {
+    const getCustomerInfoDefault = async () => {
       const res = await httpGetCustomerInfoDefault();
-      console.log(res.data);
       if (res.data) {
-        const customer = res.data;
-        setCustomer({ ...customer, id: '' });
-        setAddress({
-          provinceId: customer.provinceId,
-          districtId: customer.districtId,
-        });
-      } else setCustomer({ ...customer, provinceId: -1 });
+        let storeRes = [];
+        storeRes = await httpGetStoreByAddress(
+          res.data?.provinceId,
+          res.data?.districtId
+        );
+        console.log(storeRes);
+        if (storeRes.data?.length > 0) {
+          setStores(storeRes.data);
+          setCustomer({
+            ...res.data,
+            store_id: storeRes.data[0].id,
+          });
+        } else {
+          setStores([
+            { id: null, storeName: 'Không có cửa hàng tại địa chỉ này' },
+          ]);
+        }
+      }
     };
-    getCustomerInfo();
+    accountUsername
+      ? getCustomerInfoDefault()
+      : setCustomer({ ...customer, provinceId: -1 });
   }, []);
+
   useEffect(() => {
-    const handleAddressChange = async () => {
-      let res = [];
-      if (accountUsername) {
-        res = await httpGetStoreByAddress(
-          customer.provinceId,
-          customer.districtId
-        );
-      } else {
-        res = await httpGetStoreByAddress(
-          address.provinceId,
-          address.districtId
-        );
-      }
-      if (res.data?.length > 0) {
-        setStores(res.data);
-        setCustomer({
-          ...customer,
-          ...address,
-          store_id: res.data[0].id,
-        });
-      } else {
-        setStores([
-          { id: null, storeName: 'Không có cửa hàng tại địa chỉ này' },
-        ]);
-      }
-    };
-    handleAddressChange();
-  }, [
-    address.provinceId,
-    address.districtId,
-    address,
-    customer.districtId,
-    customer.provinceId,
-  ]);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    const { provinceId, districtId } = customer || {};
+    getListStoreByAddress(provinceId, districtId);
+  }, [customer.provinceId, customer.districtId]);
+
   const handleChange = (e) => {
     setCustomer({ ...customer, [e.target.name]: e.target.value });
   };
-  const formInputs = [
-    {
-      id: 1,
-      type: 'text',
-      name: 'name',
-      placeholder: 'Họ và tên',
-      required: true,
-    },
-    {
-      id: 2,
-      type: 'phone',
-      name: 'phone',
-      placeholder: 'Số điện thoại',
-      required: true,
-      pattern: ValidationRegex.phone.pattern,
-      message: ValidationRegex.phone.message,
-    },
-    {
-      id: 3,
-      type: 'text',
-      name: 'address',
-      placeholder: 'Địa chỉ chi tiết',
-      required: true,
-    },
-  ];
+
   return useMemo(() => {
     return (
-      customer?.provinceId && (
+      customer.provinceId && (
         <div className={cx('wrapper')}>
           {/* Modal */}
           {showModal && (
@@ -123,9 +90,11 @@ const Customer = () => {
           <div className={cx('line')}></div>
           <FormValidation>
             {({ formValidated, setValidated, setSubmitting }) => {
-               const handleSubmitCheckout = (e) => {
+              const handleSubmitCheckout = (e) => {
                 e.preventDefault();
-                formValidated && handleCheckout(setSubmitting);
+                if ((accountUsername && customer.id) || formValidated) {
+                  handleCheckout(setSubmitting);
+                }
               };
               return (
                 <form
@@ -133,55 +102,17 @@ const Customer = () => {
                   onSubmit={handleSubmitCheckout}
                 >
                   {accountUsername ? (
-                    <div className={cx('form-control')}>
-                      <h4>Địa chỉ nhận hàng</h4>
-                      <div className={cx('customer-info')}>
-                        <button
-                          type='button'
-                          onClick={() => setShowModal(true)}
-                          className={cx('list-customer')}
-                        >
-                          <Icon icon='mdi:address-marker-outline' />
-                        </button>
-                        <div className={cx('customer-info_first')}>
-                          {customer.name ? (
-                            <>
-                              <h4>{customer.name}</h4>
-                              <h5>{customer.phone || ''}</h5>
-                            </>
-                          ) : (
-                            <p>Không có địa chỉ nào được lưu</p>
-                          )}
-                        </div>
-                        <p className={cx('customer-info_last')}>
-                          {customer.address}
-                        </p>
-                      </div>
-                    </div>
+                    <Authenticated
+                      setShowModal={setShowModal}
+                      customer={customer}
+                    />
                   ) : (
-                    <>
-                      <div className={cx('form-control')}>
-                        {formInputs.map((formInput) => (
-                          <FormInput
-                            key={formInput.id}
-                            value={customer[formInput.name]}
-                            onChange={handleChange}
-                            setValidated={setValidated}
-                            {...formInput}
-                          />
-                        ))}
-                      </div>
-                      <div className={cx('form-control')}>
-                        <h4>Địa chỉ </h4>
-                        <SelectAddress
-                          address={{
-                            provinceId: customer.provinceId,
-                            districtId: customer.districtId,
-                          }}
-                          setAddress={setAddress}
-                        />
-                      </div>
-                    </>
+                    <UnAuthenticated
+                      customer={customer}
+                      setCustomer={setCustomer}
+                      handleChange={handleChange}
+                      setValidated={setValidated}
+                    />
                   )}
                   <div className={cx('form-control')}>
                     <h4>Chọn quán gần nhất</h4>
@@ -190,6 +121,7 @@ const Customer = () => {
                       name='store_id'
                       value={customer.store_id}
                       onChange={handleChange}
+                      required
                     >
                       {stores?.map((store) => (
                         <option
@@ -212,7 +144,7 @@ const Customer = () => {
         </div>
       )
     );
-  }, [address, customer.address, customer.name, customer.phone, handleChange]);
+  }, [customer.address, customer.name, customer.phone, handleChange]);
 };
 
 export default Customer;

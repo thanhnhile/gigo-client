3f53bdb9c54c970141c0c1f60f3c8b58407a0626
thanhnhile <marrythanhnhipl205@gmail.com>
2023-05-29 23:46:38 +0700

    prduct detail

--- src/components/Product/Detail.js
+++ src/components/Product/Detail.js
@@ -1,11 +1,13 @@
+import { useState, useEffect, useCallback } from 'react';
 import className from 'classnames/bind';
 import styles from './Product.module.scss';
 import { Icon } from '@iconify/react';
-import { formatPrice } from '~/utils/format';
-import { useState, useEffect } from 'react';
+
 import Clickable from '~/components/Clickable';
 import ListRating from '~/components/ReviewProduct/ListRating';
 import useCart from '~/hooks/useCart';
+import { httpGetAllTopping } from '~/apiServices/toppingServices';
+import { formatPrice } from '~/utils/format';
 const cx = className.bind(styles);
 
 const SIZE_OPTIONS = [
@@ -34,22 +36,22 @@ const SUGAR_OPTIONS = [
   },
   {
     id: 'sugar-30',
-    value: '30',
+    value: '30%',
     name: '30%',
   },
   {
     id: 'sugar-50',
-    value: '50',
+    value: '50%',
     name: '50%',
   },
   {
     id: 'sugar-70',
-    value: '70',
+    value: '70%',
     name: '70%',
   },
   {
     id: 'sugar-100',
-    value: '100',
+    value: '100%',
     name: '100%',
   },
 ];
@@ -62,30 +64,56 @@ const ICE_OPTIONS = [
   },
   {
     id: 'ice-50',
-    value: '50',
+    value: '50%',
     name: '50%',
   },
   {
     id: 'ice-100',
-    value: '100',
+    value: '100%',
     name: '100%',
   },
 ];
 
+const mapToppingOptions = (data = []) => {
+  return data
+    .filter((item) => item.status)
+    .map((item) => ({
+      id: item.id,
+      value: item.id,
+      name: `${item.name} + ${formatPrice(item.price)}`,
+    }));
+};
+
 const ProductDetail = ({ product, rates }) => {
   const [size, setSize] = useState('S');
-  const [sugar, setSugar] = useState('100');
-  const [ice, setIce] = useState('100');
+  const [sugar, setSugar] = useState('100%');
+  const [ice, setIce] = useState('100%');
+  const [toppings, setToppings] = useState([]);
+  const [toppinOptions, setToppingOptions] = useState([]);
 
   const [quantity, setQuantity] = useState(1);
   const { addToCart } = useCart();
+
+  useEffect(() => {
+    const getToppings = async () => {
+      const res = await httpGetAllTopping();
+      if (res?.data) {
+        setToppingOptions(mapToppingOptions(res.data));
+      }
+    };
+    product?.hasTopping && getToppings();
+  }, [product?.hasTopping]);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   const handlePlus = () => {
     setQuantity(quantity + 1);
   };
   const handleMinus = () => {
     quantity > 1 ? setQuantity(quantity - 1) : setQuantity(1);
   };
-  const handleChangeOption = (e) => {
+  const handleChangeOption = useCallback((e) => {
     switch (e.target.name) {
       case 'size':
         setSize(e.target.value);
@@ -94,12 +122,27 @@ const ProductDetail = ({ product, rates }) => {
         setSugar(e.target.value);
         break;
       case 'ice':
-        setSugar(e.target.value);
+        setIce(e.target.value);
         break;
       default:
         break;
     }
-  };
+  }, []);
+
+  const handleChangeToppingOption = useCallback(
+    (e) => {
+      const toppingId = Number.parseInt(e.target.value);
+      if (!toppings.includes(toppingId)) {
+        const newToppings = [...toppings];
+        newToppings.push(toppingId);
+        setToppings(newToppings);
+      } else {
+        const newToppings = toppings.filter((item) => item !== toppingId);
+        setToppings(newToppings);
+      }
+    },
+    [toppings]
+  );
 
   const handleAddToCart = () => {
     const surCharge = size === 'S' ? 0 : size === 'M' ? 6000 : 10000;
@@ -114,10 +157,7 @@ const ProductDetail = ({ product, rates }) => {
     addToCart(cartItem);
     setQuantity(1);
   };
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+  console.log(toppings);
   return (
     <div className={cx('wrapper', 'min-container')}>
       <div className={cx('flex-box', 'product-detail')}>
@@ -134,24 +174,31 @@ const ProductDetail = ({ product, rates }) => {
             title='Chọn size'
             options={SIZE_OPTIONS}
             fieldName={'size'}
-            currentValue={size}
+            checked={(value) => value === size}
             handleChange={handleChangeOption}
           />
           <SwitchField
             title='Chọn lượng đường'
             options={SUGAR_OPTIONS}
             fieldName={'sugar'}
-            currentValue={sugar}
+            checked={(value) => value === sugar}
             handleChange={handleChangeOption}
           />
           <SwitchField
             title='Chọn lượng đá'
             options={ICE_OPTIONS}
             fieldName={'ice'}
-            currentValue={ice}
+            checked={(value) => value === ice}
             handleChange={handleChangeOption}
           />
-
+          <SwitchField
+            title='Chọn topping'
+            options={toppinOptions}
+            fieldName={'toppings'}
+            checked={(value) => toppings?.includes(value)}
+            type='checkbox'
+            handleChange={handleChangeToppingOption}
+          />
           <div className={cx('product-count')}>
             <span>Số lượng</span>
             <form action='#' className={cx('display-flex')}>
@@ -186,7 +233,8 @@ const SwitchField = ({
   fieldName,
   options = [],
   handleChange,
-  currentValue,
+  checked = () => {},
+  type = 'radio',
 }) => {
   return (
     <details className={cx('product-select')}>
@@ -195,11 +243,11 @@ const SwitchField = ({
         {options.map((item) => (
           <div>
             <input
-              type='radio'
+              type={type}
               id={item.id}
               name={fieldName}
               value={item.value}
-              checked={currentValue === item.value}
+              checked={checked(item.value)}
               hidden
               onChange={handleChange}
             />
